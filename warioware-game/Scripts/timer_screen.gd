a910b314353442dcd9e14d189c16f5fafa4f9c29
Timmyaintit12 <2009pwatea@gmail.com>
2026-09-18 09:54:58 +1200

    Fixed issues with collection in platformer and with switching between levels

--- warioware-game/Scripts/timer_screen.gd
+++ warioware-game/Scripts/timer_screen.gd
@@ -14,8 +14,8 @@ func _ready() -> void:
 	await Timer(5.0)
 	
 	if Global.minigames_done < 3:
-		Global.minigames_done = Global.minigames_done +1
-		get_tree().change_scene_to_file("res://scenes/minigame_" + str(Global.minigames_done) + ".tscn")
+		Global.minigames_done = Global.minigames_done 
+		get_tree().change_scene_to_file("res://Scenes/minigame_" + str(Global.minigames_done + 1) + ".tscn")
 		
 	else:
 		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
